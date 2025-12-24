dad422b6968f03b10b5e9c62a763c4eac0a2ddec
FelipeRibeiro12 <felipe.gabriel@ges.inatel.br>
2025-12-24 02:45:06 -0300

    feat: implement authentication service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    @Inject()
+    private readonly authService: AuthService;
+
+    // responsavel pelo gerenciamento das rotas de autenticacao
+
+    /*
+    signIn()
+    signUp()
+    */
+
+    // metodo para login
+    @Post('signin') // rota para login - auth/signin
+    async signIn(@Body() body: Prisma.UserCreateInput) { // rota para login - auth/signin // qual o tipo do body? // Prisma.UserCreateInput
+        return this.authService.signIn(body);
+        // maria ss22
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    // todo: implementar servicos de autenticacao
+    @Inject()
+    private readonly userService: UserService;
+
+    async signIn(params: Prisma.UserCreateInput
+    ): Promise<Omit<User, 'password'>> { // retorna o usuario sem a senha para o retorno da funcao
+    const user = await this.userService.findOne({ email: params.email }); // buscando usuario pelo email
+    if (!user) // se usuario nao encontrado
+      throw new NotFoundException('User not found'); 
+
+    const passwordMatch: boolean = await bcrypt.compare(params.password, user.password); // comparando senhas
+    if (!passwordMatch) // se senha nao bate
+      throw new UnauthorizedException('Invalid credentials');
+
+    const { password, ...result } = user; // removendo senha do objeto retornado, e o resto vai para result
+
+    // todo: criar e retornar o jwt aqui
+
+    return result;
+  }
+}
